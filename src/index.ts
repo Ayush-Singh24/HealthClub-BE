cd7e8ce310d074e3993b149003bc45e19df2a5c3
Ayush Singh <asayushsingh80@gmail.com>
2024-03-03 18:18:35 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Express } from "express";
+import { config } from "dotenv";
+config();
+import cors, { CorsOptions } from "cors";
+
+const PORT = process.env.PORT || 5000;
+
+const app: Express = express();
+
+const whitelist = ["http://localhost:3000", "http://localhost:8000"];
+const corsOptions: CorsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin) {
+      return callback(null, true);
+    }
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by cors"));
+    }
+  },
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}🚀`);
+});
