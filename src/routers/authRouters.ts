e82eecd78edf3f5b1ae508136c0d1a1915af2934
Ayush Singh <asayushsingh80@gmail.com>
2024-03-05 02:47:43 +0530

    authRouter: implemented login route

--- src/routers/authRouters.ts
+++ src/routers/authRouters.ts
@@ -1,8 +1,8 @@
 import express, { Router, Request, Response } from "express";
 import { upload } from "../middlewares/imageHandler.js";
 import { IMAGE } from "../utils/constants.js";
-import { signUpScema } from "../utils/zodSchemas.js";
-import { signUpUser } from "../services/authServices.js";
+import { loginScheme, signUpScema } from "../utils/zodSchemas.js";
+import { loginUser, signUpUser } from "../services/authServices.js";
 
 export const authRouter: Router = express.Router();
 
@@ -36,9 +36,19 @@ authRouter.post(
         },
         req.file.path
       );
-      res.status(200).send({ message: "Signed Up" });
+      res.status(201).send({ message: "Signed Up" });
     } catch (error) {
       console.log(error);
     }
   }
 );
+
+authRouter.post("/login", async (req: Request, res: Response) => {
+  try {
+    const { username, password } = loginScheme.parse(req.body);
+    await loginUser({ username, password });
+    res.status(200).send({ message: "Logged In" });
+  } catch (error) {
+    console.log(error);
+  }
+});
